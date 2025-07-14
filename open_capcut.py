b7d70fe7fc2de85ab0d85c6ceab208d3410b5860
dreamsncode <power.of.uniqueness@gmail.com>
2025-07-14 22:36:56 +0530

    Add files via upload

---
+++ open_capcut.py
@@ -0,0 +1,13 @@
+import os
+import time
+import pyautogui
+
+# Launch CapCut
+os.startfile("C:\\Users\\Admin\\AppData\\Local\\CapCut\\Apps\\CapCut.exe")
+
+time.sleep(10)
+
+print(" Please  Wait Capcut Is Opening ")
+
+pyautogui.moveTo(500, 300)
+pyautogui.click()
